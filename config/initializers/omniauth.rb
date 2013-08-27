ac27b5c489f147b427acba8eed08daf46d30f22b
Sanyo <paresharma@gmail.com>
2013-08-28 02:47:10 +0530

    init

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, '3H4PYBZDzdAbLaYEoo7w', 'pZCQ6s8e1onbHlvcWN63bIZELrWCG8imGOegMIs'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitRep::Application.config.secret_token = '19123b9aff30e27142ac01978ea8a36637382d5a9b9b6d9949bbc5bcc1c9886dbbbed70d02e3d9c891319aabaf51005ebdf64ec66a891e14dae85116252617c4'
